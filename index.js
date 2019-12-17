5654b4809348b69525495bf114dcaa2047263b33
kennansalisbury <kennansalisbury@gmail.com>
2019-12-16 16:04:22 -0800

    setup routes, added pics

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const layouts = require('express-ejs-layouts')
 
 const app = express()
+app.use(layouts)
+app.use('/shops', require('./controllers/shops'))
+app.use(express.urlencoded({ extended:false }))
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('home')
+    res.render('home')
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('listening')
+    console.log('🌷🌷🌷🌷')
 })
